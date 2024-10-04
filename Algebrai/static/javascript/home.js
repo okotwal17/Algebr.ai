224af05683005ee2a37fa8ee55b1e33cdb1d3207
okotwal <156717048+okotwal@users.noreply.github.com>
2024-10-04 17:56:07 +0000

    Made our animations and our css better for home page.

--- Algebrai/static/javascript/home.js
+++ Algebrai/static/javascript/home.js
@@ -1,65 +1,49 @@
+// home.js
+
 // Swipe gesture detection for mobile users
 const swipeSections = document.querySelectorAll('.swipe-section');
 let startX, endX;
 
-
 function handleTouchStart(event) {
-   startX = event.touches[0].clientX;
+    startX = event.touches[0].clientX;
 }
-
 
 function handleTouchMove(event) {
-   endX = event.touches[0].clientX;
+    endX = event.touches[0].clientX;
 }
-
 
 function handleTouchEnd(event) {
-   if (startX > endX + 50) {
-       // Swipe left to the next section
-       nextSection();
-   } else if (startX < endX - 50) {
-       // Swipe right to the previous section
-       previousSection();
-   }
+    if (startX > endX + 50) {
+        // Swipe left to the next section
+        nextSection();
+    } else if (startX < endX - 50) {
+        // Swipe right to the previous section
+        previousSection();
+    }
 }
 
-
 swipeSections.forEach(section => {
-   section.addEventListener('touchstart', handleTouchStart);
-   section.addEventListener('touchmove', handleTouchMove);
-   section.addEventListener('touchend', handleTouchEnd);
+    section.addEventListener('touchstart', handleTouchStart);
+    section.addEventListener('touchmove', handleTouchMove);
+    section.addEventListener('touchend', handleTouchEnd);
 });
-
 
 let currentSectionIndex = 0;
 
-
 function nextSection() {
-   if (currentSectionIndex < swipeSections.length - 1) {
-       currentSectionIndex++;
-       scrollToSection(currentSectionIndex);
-   }
+    if (currentSectionIndex < swipeSections.length - 1) {
+        currentSectionIndex++;
+        scrollToSection(currentSectionIndex);
+    }
 }
-
 
 function previousSection() {
-   if (currentSectionIndex > 0) {
-       currentSectionIndex--;
-       scrollToSection(currentSectionIndex);
-   }
+    if (currentSectionIndex > 0) {
+        currentSectionIndex--;
+        scrollToSection(currentSectionIndex);
+    }
 }
-
 
 function scrollToSection(index) {
-   swipeSections[index].scrollIntoView({ behavior: 'smooth' });
+    swipeSections[index].scrollIntoView({ behavior: 'smooth' });
 }
-
-
-
-
-document.addEventListener("DOMContentLoaded", function() {
-   const username = prompt("Please enter your name:");
-   if (username) {
-       document.getElementById("username").textContent = username;
-   }
-});
