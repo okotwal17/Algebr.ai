5e79ad324c910c40fe87554b80ae8390957892c3
okotwal <156717048+okotwal@users.noreply.github.com>
2024-11-15 17:13:05 +0000

    Continuing to work on the front-end.

--- accounts/static/javascript/login.js
+++ accounts/static/javascript/login.js
@@ -3,14 +3,15 @@ document.addEventListener("DOMContentLoaded", () => {
     background.classList.add("background");
     document.body.appendChild(background);
 
-    for (let i = 0; i < 20; i++) {
+    // Generate bubbles
+    for (let i = 0; i < 30; i++) {
         const bubble = document.createElement("div");
         bubble.classList.add("bubble");
 
-        bubble.style.setProperty("--size", `${Math.random() * 5 + 5}rem`);
+        bubble.style.setProperty("--size", `${Math.random() * 4 + 4}rem`);
         bubble.style.setProperty("--position", `${Math.random() * 100}%`);
-        bubble.style.setProperty("--time", `${Math.random() * 3 + 2}s`);
-        bubble.style.setProperty("--delay", `${Math.random() * 3}s`);
+        bubble.style.setProperty("--time", `${Math.random() * 6 + 4}s`);
+        bubble.style.setProperty("--delay", `${Math.random() * 4}s`);
 
         background.appendChild(bubble);
     }
